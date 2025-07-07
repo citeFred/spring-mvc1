904a5a903f21556dba78b158beeb34a8f48ee11d
citeFred <citefred@gmail.com>
2025-07-07 12:38:07 +0900

    feat: 뷰템플릿엔진 - Model 객체를 통한 동적데이터바인딩

--- src/main/java/com/metaverse/springmvc/controller/HelloViewController.java
+++ src/main/java/com/metaverse/springmvc/controller/HelloViewController.java
@@ -1,10 +1,13 @@
 package com.metaverse.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloViewController {
+    private static long visitCount = 0;
+
     @GetMapping("/static-hello")
     public String hello() {
         return "hello.html";
@@ -21,5 +24,12 @@ public class HelloViewController {
         // prefix classpath: resources까지의 경로 + templates/ 까지 가지고있음
         // suffix .html
         return "hello-template";
+    }
+
+    @GetMapping("html/dynamic")
+    public String htmlDynamic(Model model) {
+        visitCount++;
+        model.addAttribute("visits", visitCount);
+        return "hello-dynamic";
     }
 }
